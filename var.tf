9a334031d3aa5d1812d99418fcc4ffbc0897c48f
chandanpathakadityagit <165925379+chandanpathakadityagit@users.noreply.github.com>
2024-04-03 20:27:11 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  description = "The GCP project ID"
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+}
+
+variable "region" {
+  description = "The GCP region for the VPC"
+}
