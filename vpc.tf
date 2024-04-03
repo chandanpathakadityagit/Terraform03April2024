c7eb011ce03c483a2cddd982ba6c59c08bb310fc
chandanpathakadityagit <165925379+chandanpathakadityagit@users.noreply.github.com>
2024-04-03 20:27:41 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.vpc_name}-subnet"
+  ip_cidr_range = var.vpc_cidr
+  network       = google_compute_network.vpc.self_link
+  region        = var.region
+}
